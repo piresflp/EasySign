8332806aa0161d22f908b2b85d3236f535e7bfe7
Rafael borrozino <rafa.rborroz@gmail.com>
2021-06-01 08:20:11 -0300

    home pronta (cores a decidir)

---
+++ easy_sign/lib/Screens/Dicio/dicio.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class Dicio extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- easy_sign/lib/main.dart
+++ easy_sign/lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:easy_sign/constants.dart';
-import 'model/category.dart';
+//import 'constants.dart';
+//import 'model/category.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+//import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+//import 'package:flutter_svg/flutter_svg.dart';
+import 'package:easy_sign/Screens/Home/home.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,14 +15,17 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'EasySign',
-      theme: ThemeData(),
-      home: HomeScreen(),
+      theme: ThemeData(
+        primaryColor: kPrimaryColor,
+        scaffoldBackgroundColor: Colors.white,
+      ),
+      home: Home(),
       debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class HomeScreen extends StatelessWidget {
+/*class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -114,4 +119,4 @@ class HomeScreen extends StatelessWidget {
       ),
     );
   }
-}
+}*/

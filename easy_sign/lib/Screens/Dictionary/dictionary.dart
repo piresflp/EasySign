a805cd4ac40a6fd2abc6019b3c5fdec6310e8b9e
Felipe Pires <flpiresa@gmail.com>
2021-06-30 16:39:58 -0300

    fixed buttons

---
+++ easy_sign/lib/Screens/Dictionary/dictionary.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class Dictionary extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- easy_sign/lib/Screens/Translator/translator.dart
+++ easy_sign/lib/Screens/Translator/translator.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class Dicio extends StatelessWidget {
+class Translator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container();
